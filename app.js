d47c4307de84e1eeece67ef274e9da464af63d37
kaxxsh <kameshwaran2002@gmail.com>
2023-07-21 22:41:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+import Express from "express";
+import dbConnection from "./db/dbConnection.js";
+import {} from "dotenv/config.js";
+import errorHandler from "./Middleware/error-handler.js";
+import cors from "cors";
+
+const app = Express();
+app.use(cors());
+
+app.use(Express.json());
+app.use(errorHandler);
+
+const connection = () => {
+  try {
+    dbConnection(process.env.MONGO_URL);
+    app.listen(3001, (req, res) => {
+      console.log("Server is running at port 3001");
+    });
+  } catch (error) {
+    console.log("Error in connecting to DB", error);
+  }
+};
+
+connection();
